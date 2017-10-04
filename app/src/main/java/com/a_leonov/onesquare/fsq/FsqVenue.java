1bf00e7e9ca066aab3d9042a1906d866ff3f23f5
melle095 <melle095@gmail.com>
2017-10-04 17:18:46 +0300

    Adding fsq api

---
+++ app/src/main/java/com/a_leonov/onesquare/fsq/FsqVenue.java
@@ -0,0 +1,16 @@
+package com.a_leonov.onesquare.fsq;
+
+import android.location.Location;
+
+
+public class FsqVenue {
+    public String id;
+    public String name;
+    public String address;
+    public String type;
+    public Location location;
+    public int direction;
+    public int distance;
+    public int herenow;
+
+}
