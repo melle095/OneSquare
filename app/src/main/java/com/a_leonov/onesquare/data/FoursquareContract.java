c2fa61084970c42a14daf515b20263ba4dabc28d
melle095 <melle095@gmail.com>
2017-10-17 00:08:41 +0300

    contracts classes

---
+++ app/src/main/java/com/a_leonov/onesquare/data/FoursquareContract.java
@@ -0,0 +1,86 @@
+package com.a_leonov.onesquare.data;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+
+public class FoursquareContract {
+    public static final String CONTENT_AUTHORITY = "com.a_leonov.onesquare.app";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_VENUES = "venues";
+    public static final String PATH_LOCATION = "location";
+
+    public static final class VenuesEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_VENUES).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_VENUES;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_VENUES;
+
+        public static final String TABLE_NAME = "venues";
+
+        public static final String COLUMN_VEN_KEY = "venue_id";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_PHONE = "phone";
+        public static final String COLUMN_FORMATTEDPHONE = "formattedPhone";
+        public static final String COLUMN_TWITTER = "twitter";
+        public static final String COLUMN_INSTAGRAMM = "instagram";
+        public static final String COLUMN_FACEBOOK = "facebook";
+        public static final String COLUMN_FACEBOOKUSER = "facebookUsername";
+        public static final String COLUMN_FACEBOOKNAME = "facebookName";
+        public static final String COLUMN_LOCATION = "location";
+        public static final String COLUMN_VERIFIED = "verified";
+        public static final String COLUMN_URL = "url";
+        public static final String COLUMN_STATUS = "hours";
+        public static final String COLUMN_ISOPEN = "isOpen";
+        public static final String COLUMN_ISLOCALHOLIDAY = "isLocalHoliday";
+        public static final String COLUMN_POPULAR = "popular";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_RATING = "rating";
+        public static final String COLUMN_PHOTOS = "photos";
+        public static final String COLUMN_SHORTURL = "shortUrl";
+        public static final String COLUMN_CANONICALURL = "canonicalUrl";
+
+        public static Uri buildVenuesUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+    }
+
+    public static final class LocationEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_LOCATION).build();
+
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_LOCATION;
+
+        // Table name
+        public static final String TABLE_NAME = "location";
+
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        public static final String COLUMN_ADDRESS = "address";
+        public static final String COLUMN_CROSSSTREET = "crossStreet";
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+        public static final String COLUMN_COORD_LONG = "coord_long";
+        public static final String COLUMN_POSTALCODE = "postal_code";
+        public static final String COLUMN_CC = "country_code";
+        public static final String COLUMN_CITY = "city";
+        public static final String COLUMN_STATE = "state";
+        public static final String COLUMN_COUNTRY = "country";
+        
+        public static Uri buildLocationUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/a_leonov/onesquare/data/FoursquareDbHelper.java
@@ -0,0 +1,10 @@
+package com.a_leonov.onesquare.data;
+
+import SQLiteOpenHelper;
+
+/**
+ * Created by Лена on 14.10.2017.
+ */
+
+public class FoursquareDbHelper extends SQLiteOpenHelper {
+}
