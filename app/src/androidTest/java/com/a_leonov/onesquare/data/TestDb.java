7a45f850f73d5372b4417beb784298b2fea1e439
melle095 <melle095@gmail.com>
2017-10-23 16:52:26 +0300

    Test complete

--- app/src/androidTest/java/com/a_leonov/onesquare/data/TestDb.java
+++ app/src/androidTest/java/com/a_leonov/onesquare/data/TestDb.java
@@ -13,6 +13,7 @@ import java.util.HashSet;
 
 public class TestDb extends AndroidTestCase {
     public static final String LOG_TAG = TestDb.class.getSimpleName();
+    static final String TEST_VENUE_ID = "412d2800f964a520df0c1fe3";
 
     // Since we want each test to start with a clean slate
     void deleteTheDatabase() {
@@ -28,7 +29,7 @@ public class TestDb extends AndroidTestCase {
         // Note that there will be another table in the DB that stores the
         // Android metadata (db version information)
         final HashSet<String> tableNameHashSet = new HashSet<String>();
-        tableNameHashSet.add(FoursquareContract.LocationEntry.TABLE_NAME);
+        tableNameHashSet.add(FoursquareContract.PhotoEntry.TABLE_NAME);
         tableNameHashSet.add(FoursquareContract.VenuesEntry.TABLE_NAME);
 
         mContext.deleteDatabase(FoursquareDbHelper.DATABASE_NAME);
@@ -53,55 +54,47 @@ public class TestDb extends AndroidTestCase {
                 tableNameHashSet.isEmpty());
 
         // now, do our tables contain the correct columns?
-        c = db.rawQuery("PRAGMA table_info(" + FoursquareContract.LocationEntry.TABLE_NAME + ")",
+        c = db.rawQuery("PRAGMA table_info(" + FoursquareContract.VenuesEntry.TABLE_NAME + ")",
                 null);
 
         assertTrue("Error: This means that we were unable to query the database for table information.",
                 c.moveToFirst());
 
         // Build a HashSet of all of the column names we want to look for
-        final HashSet<String> locationColumnHashSet = new HashSet<String>();
-        locationColumnHashSet.add(FoursquareContract.LocationEntry._ID);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_ADDRESS);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_CC);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_CITY);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_COORD_LAT);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_COORD_LONG);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_COUNTRY);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_CROSSSTREET);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_POSTALCODE);
-        locationColumnHashSet.add(FoursquareContract.LocationEntry.COLUMN_STATE);
+        final HashSet<String> venuesColumnHashSet = new HashSet<String>();
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry._ID);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_ADDRESS);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_CC);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_CITY);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_COORD_LAT);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_COORD_LONG);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_COUNTRY);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_CROSSSTREET);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_POSTALCODE);
+        venuesColumnHashSet.add(FoursquareContract.VenuesEntry.COLUMN_STATE);
 
         int columnNameIndex = c.getColumnIndex("name");
         do {
             String columnName = c.getString(columnNameIndex);
-            locationColumnHashSet.remove(columnName);
+            venuesColumnHashSet.remove(columnName);
         } while(c.moveToNext());
 
         // if this fails, it means that your database doesn't contain all of the required location
         // entry columns
         assertTrue("Error: The database doesn't contain all of the required location entry columns",
-                locationColumnHashSet.isEmpty());
+                venuesColumnHashSet.isEmpty());
         db.close();
     }
 
-    public void testLocationTable() {
-        insertLocation();
+    public void testVenuesTable() {
+        insertVenue();
     }
 
-    public void testWeatherTable() {
-        // First insert the location, and then use the locationRowId to insert
-        // the weather. Make sure to cover as many failure cases as you can.
-
-        // Instead of rewriting all of the code we've already written in testLocationTable
-        // we can move this code to insertLocation and then call insertLocation from both
-        // tests. Why move it? We need the code to return the ID of the inserted location
-        // and our testLocationTable can only return void because it's a test.
-
-        long locationRowId = insertLocation();
+    public void testPhotoTable() {
+        long venueRowId = insertVenue();
 
         // Make sure we have a valid row ID.
-        assertFalse("Error: Location Not Inserted Correctly", locationRowId == -1L);
+        assertFalse("Error: Location Not Inserted Correctly", venueRowId == -1L);
 
         // First step: Get reference to writable database
         // If there's an error in those massive SQL table creation Strings,
@@ -110,16 +103,16 @@ public class TestDb extends AndroidTestCase {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         // Second Step (Weather): Create weather values
-        ContentValues weatherValues = TestUtilities.createVenuesValues(locationRowId);
+        ContentValues photoValues = TestUtilities.createPhotoValues(TEST_VENUE_ID);
 
         // Third Step (Weather): Insert ContentValues into database and get a row ID back
-        long weatherRowId = db.insert(FoursquareContract.VenuesEntry.TABLE_NAME, null, weatherValues);
-        assertTrue(weatherRowId != -1);
+        long photoRowId = db.insert(FoursquareContract.PhotoEntry.TABLE_NAME, null, photoValues);
+        assertTrue( photoRowId != -1);
 
         // Fourth Step: Query the database and receive a Cursor back
         // A cursor is your primary interface to the query results.
-        Cursor weatherCursor = db.query(
-                FoursquareContract.VenuesEntry.TABLE_NAME,  // Table to Query
+        Cursor photoCursor = db.query(
+                FoursquareContract.PhotoEntry.TABLE_NAME,  // Table to Query
                 null, // leaving "columns" null just returns all the columns.
                 null, // cols for "where" clause
                 null, // values for "where" clause
@@ -129,52 +122,38 @@ public class TestDb extends AndroidTestCase {
         );
 
         // Move the cursor to the first valid database row and check to see if we have any rows
-        assertTrue( "Error: No Records returned from location query", weatherCursor.moveToFirst() );
+        assertTrue( "Error: No Records returned from location query", photoCursor.moveToFirst() );
 
         // Fifth Step: Validate the location Query
-        TestUtilities.validateCurrentRecord("testInsertReadDb weatherEntry failed to validate",
-                weatherCursor, weatherValues);
+        TestUtilities.validateCurrentRecord("testInsertReadDb photoEntry failed to validate",
+                photoCursor, photoValues);
 
         // Move the cursor to demonstrate that there is only one record in the database
         assertFalse( "Error: More than one record returned from weather query",
-                weatherCursor.moveToNext() );
+                photoCursor.moveToNext() );
 
         // Sixth Step: Close cursor and database
-        weatherCursor.close();
+        photoCursor.close();
         dbHelper.close();
     }
 
 
-    /*
-        Students: This is a helper method for the testWeatherTable quiz. You can move your
-        code from testLocationTable to here so that you can call this code from both
-        testWeatherTable and testLocationTable.
-     */
-    public long insertLocation() {
-        // First step: Get reference to writable database
-        // If there's an error in those massive SQL table creation Strings,
-        // errors will be thrown here when you try to get a writable database.
+
+    public long insertVenue() {
+
         FoursquareDbHelper dbHelper = new FoursquareDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        // Second Step: Create ContentValues of what you want to insert
-        // (you can use the createNorthPoleLocationValues if you wish)
-        ContentValues testValues = TestUtilities.createMoscowLocationValues();
+        ContentValues venuesValues = TestUtilities.createVenuesValues(TEST_VENUE_ID);
 
-        // Third Step: Insert ContentValues into database and get a row ID back
-        long locationRowId;
-        locationRowId = db.insert(FoursquareContract.LocationEntry.TABLE_NAME, null, testValues);
+        long venueRowId;
+        venueRowId = db.insert(FoursquareContract.VenuesEntry.TABLE_NAME, null, venuesValues);
 
         // Verify we got a row back.
-        assertTrue(locationRowId != -1);
+        assertTrue(venueRowId != -1);
 
-        // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
-        // the round trip.
-
-        // Fourth Step: Query the database and receive a Cursor back
-        // A cursor is your primary interface to the query results.
         Cursor cursor = db.query(
-                FoursquareContract.LocationEntry.TABLE_NAME,  // Table to Query
+                FoursquareContract.VenuesEntry.TABLE_NAME,  // Table to Query
                 null, // all columns
                 null, // Columns for the "where" clause
                 null, // Values for the "where" clause
@@ -191,7 +170,7 @@ public class TestDb extends AndroidTestCase {
         // (you can use the validateCurrentRecord function in TestUtilities to validate the
         // query if you like)
         TestUtilities.validateCurrentRecord("Error: Location Query Validation Failed",
-                cursor, testValues);
+                cursor, venuesValues);
 
         // Move the cursor to demonstrate that there is only one record in the database
         assertFalse( "Error: More than one record returned from location query",
@@ -200,6 +179,6 @@ public class TestDb extends AndroidTestCase {
         // Sixth Step: Close Cursor and Database
         cursor.close();
         db.close();
-        return locationRowId;
+        return venueRowId;
     }
 }
