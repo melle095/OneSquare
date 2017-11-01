c6a89719786b1dd8a476eae357f7847f84435321
melle095 <melle095@gmail.com>
2017-11-02 00:55:07 +0300

    rtet

--- app/src/main/java/com/a_leonov/onesquare/data/VenueProvider.java
+++ app/src/main/java/com/a_leonov/onesquare/data/VenueProvider.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteQueryBuilder;
+import android.graphics.PointF;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -109,4 +110,35 @@ public class VenueProvider extends ContentProvider {
         return matcher;
     }
 
+
+    public static PointF calculateDerivedPosition(PointF point,
+                                                  double range, double bearing)
+    {
+        double EarthRadius = 6371000; // m
+
+        double latA = Math.toRadians(point.x);
+        double lonA = Math.toRadians(point.y);
+        double angularDistance = range / EarthRadius;
+        double trueCourse = Math.toRadians(bearing);
+
+        double lat = Math.asin(
+                Math.sin(latA) * Math.cos(angularDistance) +
+                        Math.cos(latA) * Math.sin(angularDistance)
+                                * Math.cos(trueCourse));
+
+        double dlon = Math.atan2(
+                Math.sin(trueCourse) * Math.sin(angularDistance)
+                        * Math.cos(latA),
+                Math.cos(angularDistance) - Math.sin(latA) * Math.sin(lat));
+
+        double lon = ((lonA + dlon + Math.PI) % (Math.PI * 2)) - Math.PI;
+
+        lat = Math.toDegrees(lat);
+        lon = Math.toDegrees(lon);
+
+        PointF newPoint = new PointF((float) lat, (float) lon);
+
+        return newPoint;
+
+    }
 }
