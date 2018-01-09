c439abf61621ace8d6756d1b548382bf527087a4
melle095 <melle095@gmail.com>
2018-01-09 18:33:32 +0300

    Create ui

--- app/src/main/java/com/a_leonov/onesquare/ui/DetailActivity.java
+++ app/src/main/java/com/a_leonov/onesquare/ui/DetailActivity.java
@@ -18,6 +18,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.a_leonov.onesquare.R;
 import com.a_leonov.onesquare.data.FoursquareContract;
 import com.a_leonov.onesquare.service.VenueDetailsService;
@@ -39,15 +40,15 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
             FoursquareContract.VenuesEntry.COLUMN_VEN_KEY
     };
 
-    static final int COL_ID         = 0;
-    static final int COL_NAME       = 1;
-    static final int COL_DISTANCE   = 2;
-    static final int COL_ADDRESS    = 3;
-    static final int COL_PHONE      = 4;
-    static final int COL_TWITTER    = 5;
+    static final int COL_ID = 0;
+    static final int COL_NAME = 1;
+    static final int COL_DISTANCE = 2;
+    static final int COL_ADDRESS = 3;
+    static final int COL_PHONE = 4;
+    static final int COL_TWITTER = 5;
     static final int COL_INSTAGRAMM = 6;
-    static final int COL_FACEBOOK   = 7;
-    static final int COL_VENKEY     = 8;
+    static final int COL_FACEBOOK = 7;
+    static final int COL_VENKEY = 8;
 
     private int COL_PHOTO_VENUE_ID = 0;
     private int COL_PHOTO_HEIGHT = 1;
@@ -57,7 +58,7 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
     private int COL_PHOTO_PHOTO_ID = 5;
 
     private ViewPager viewPager;
-    private CursorFragmentPagerAdapter cursorFragmentPagerAdapter;
+    private MyFragmentPagerAdapter myFragmentPagerAdapter;
     private ImageView venue_Photo;
     private TextView venue_title;
     private TextView venue_distance;
@@ -66,19 +67,18 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
     private TextView venue_twitter;
     private TextView venue_instagramm;
     private TextView venue_facebook;
-    private final String DETAIL_TAG     = "vendb_id";
-    private final String VEN_ID         = "ven_id";
+    private final String DETAIL_TAG = "vendb_id";
+    private final String VEN_ID = "ven_id";
 
     public static final String EXTENDED_DATA_STATUS =
             "com.a_leonov.onesquare.STATUS";
     public static final String BROADCAST_ACTION =
             "com.a_leonov.onesquare.BROADCAST";
 
-    private final int DETAIL_LOADER     = 3;
-    private final int DETAIL_PHOTO_LOADER     = 4;
+    private final int DETAIL_LOADER = 3;
+    private final int DETAIL_PHOTO_LOADER = 4;
 
     private long venueDbID;
-
 
 
     @Override
@@ -87,21 +87,21 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
         setContentView(R.layout.activity_article_detail);
 
 //      venue_Photo      = findViewById(R.id.photo);
-        viewPager        = findViewById(R.id.viewPager);
+        viewPager = findViewById(R.id.viewPager);
 //        venue_title         = findViewById(R.id.venue_title);
-        venue_distance   = findViewById(R.id.venue_distance);
-        venue_address    = findViewById(R.id.venue_address);
-        venue_phone      = findViewById(R.id.venue_phone);
-        venue_twitter    = findViewById(R.id.venue_twitter);
+        venue_distance = findViewById(R.id.venue_distance);
+        venue_address = findViewById(R.id.venue_address);
+        venue_phone = findViewById(R.id.venue_phone);
+        venue_twitter = findViewById(R.id.venue_twitter);
         venue_instagramm = findViewById(R.id.venue_instagramm);
-        venue_facebook   = findViewById(R.id.venue_facebook);
+        venue_facebook = findViewById(R.id.venue_facebook);
 
         getSupportLoaderManager().initLoader(DETAIL_LOADER, null, this);
 
+        myFragmentPagerAdapter =
+                new MyFragmentPagerAdapter(getSupportFragmentManager());
 
-        cursorFragmentPagerAdapter =
-                new CursorFragmentPagerAdapter(getSupportFragmentManager(), this, venueDbID);
-        viewPager.setAdapter(cursorFragmentPagerAdapter);
+        viewPager.setAdapter(myFragmentPagerAdapter);
         viewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
@@ -129,17 +129,17 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
                 statusIntentFilter);
     }
 
-    @Override
-    public void onBackPressed() {
-        if (viewPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
-        }
-    }
+//    @Override
+//    public void onBackPressed() {
+//        if (viewPager.getCurrentItem() == 0) {
+//            // If the user is currently looking at the first step, allow the system to handle the
+//            // Back button. This calls finish() on this activity and pops the back stack.
+//            super.onBackPressed();
+//        } else {
+//            // Otherwise, select the previous step.
+//            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+//        }
+//    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -152,8 +152,8 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
                             null,
                             null,
                             null);
-                } else
-                    return null;
+                }
+                return null;
             }
             case DETAIL_PHOTO_LOADER: {
                 Uri photoUri = FoursquareContract.PhotoEntry.buildPhotoByVenueUri(venueDbID);
@@ -177,25 +177,41 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
 //        Log.d("DetailActivity", DatabaseUtils.dumpCursorToString(data));
                 //venue_Photo = null;
 
-                venue_distance.setText(data.isNull(COL_DISTANCE) ? "no data" : data.getString(COL_DISTANCE));
-                venue_address.setText(data.isNull(COL_ADDRESS) ? "no data" : data.getString(COL_ADDRESS));
-                venue_phone.setText(data.isNull(COL_PHONE) ? "no data" : data.getString(COL_PHONE));
-                venue_twitter.setText(data.isNull(COL_TWITTER) ? "no data" : data.getString(COL_TWITTER));
-                venue_instagramm.setText(data.isNull(COL_INSTAGRAMM) ? "no data" : data.getString(COL_INSTAGRAMM));
-                venue_facebook.setText(data.isNull(COL_FACEBOOK) ? "no data" : data.getString(COL_FACEBOOK));
+//                venue_distance.setText(data.isNull(COL_DISTANCE) ? "no data" : data.getString(COL_DISTANCE));
+//                venue_address.setText(data.isNull(COL_ADDRESS) ? "no data" : data.getString(COL_ADDRESS));
+//                venue_phone.setText(data.isNull(COL_PHONE) ? "no data" : data.getString(COL_PHONE));
+//                venue_twitter.setText(data.isNull(COL_TWITTER) ? "no data" : data.getString(COL_TWITTER));
+//                venue_instagramm.setText(data.isNull(COL_INSTAGRAMM) ? "no data" : data.getString(COL_INSTAGRAMM));
+//                venue_facebook.setText(data.isNull(COL_FACEBOOK) ? "no data" : data.getString(COL_FACEBOOK));
+
+
+                venue_distance.setText(data.getString(COL_DISTANCE));
+                venue_address.setText(data.getString(COL_ADDRESS));
+                venue_phone.setText(data.getString(COL_PHONE));
+                venue_twitter.setText(data.getString(COL_TWITTER));
+                venue_instagramm.setText(data.getString(COL_INSTAGRAMM));
+                venue_facebook.setText(data.getString(COL_FACEBOOK));
+
 
                 Intent intentPhotos = new Intent(this, VenueDetailsService.class)
                         .putExtra(VEN_ID, data.getString(COL_VENKEY))
                         .putExtra(DETAIL_TAG, String.valueOf(venueDbID));
 //
                 startService(intentPhotos);
+                break;
             }
             case DETAIL_PHOTO_LOADER: {
-                ArrayList<String> photosList = new ArrayList<>();
-                Log.i(getClass().getSimpleName(),DatabaseUtils.dumpCursorToString(data));
 
-                while (data.moveToNext()) {
-                    photosList.add(data.getString(COL_PHOTO_PREFIX) + "width500" + data.getString(COL_PHOTO_SUFFIX));
+                if (data != null) {
+                    Log.i(getLocalClassName(), DatabaseUtils.dumpCursorToString(data));
+
+                    ArrayList<String> photosList = new ArrayList<>();
+                    while (data.moveToNext()) {
+                        photosList.add(data.getString(COL_PHOTO_PREFIX) + "width500" + data.getString(COL_PHOTO_SUFFIX));
+                    }
+                    myFragmentPagerAdapter.setData(photosList);
+                    myFragmentPagerAdapter.notifyDataSetChanged();
+                    break;
                 }
             }
         }
@@ -216,16 +232,16 @@ public class DetailActivity extends AppCompatActivity implements SwipeRefreshLay
 
     }
 
-    private class DownloadStateReceiver extends BroadcastReceiver
-    {
+    private class DownloadStateReceiver extends BroadcastReceiver {
         // Prevents instantiation
         private DownloadStateReceiver() {
         }
+
         // Called when the BroadcastReceiver gets an Intent it's registered to receive
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            if (intent.getBooleanExtra(EXTENDED_DATA_STATUS,false)) {
+            if (intent.getBooleanExtra(EXTENDED_DATA_STATUS, false)) {
                 Log.i(getLocalClassName(), "Venue detail service broadcast!");
 //                Log.i(getLocalClassName(), DatabaseUtils.dumpCursorToString(photoCur));
             }
--- app/src/main/java/com/a_leonov/onesquare/ui/FragmentPager.java
+++ app/src/main/java/com/a_leonov/onesquare/ui/FragmentPager.java
@@ -1,22 +1,16 @@
 package com.a_leonov.onesquare.ui;
 
-import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.util.Log;
+import android.support.v7.graphics.Palette;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
 import com.a_leonov.onesquare.R;
-import com.a_leonov.onesquare.data.FoursquareContract;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
@@ -24,18 +18,20 @@ import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
-public class FragmentPager extends Fragment {
+public class FragmentPager extends Fragment{
 
     private ImageView imageView;
+    public static final String ARG_URL = "url";
     public static final String ARG_PAGE = "page";
     private int mPageNumber;
     private String imageUrl;
 
 
-    public static FragmentPager init(String imageUrl) {
+    public static FragmentPager init(String imageUrl, int mPageNumber) {
         FragmentPager fragment = new FragmentPager();
         Bundle args = new Bundle();
-        args.putString(ARG_PAGE, imageUrl);
+        args.putString(ARG_URL, imageUrl);
+        args.putInt(ARG_PAGE, mPageNumber);
         fragment.setArguments(args);
         return fragment;
     }
@@ -47,8 +43,9 @@ public class FragmentPager extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments().containsKey(ARG_PAGE)) {
-            this.imageUrl = getArguments().getString(ARG_PAGE);
+        if (getArguments().containsKey(ARG_PAGE)&& getArguments().containsKey(ARG_URL)) {
+            this.mPageNumber = getArguments().getInt(ARG_PAGE);
+            this.imageUrl = getArguments().getString(ARG_URL);
         }
     }
 
@@ -60,38 +57,38 @@ public class FragmentPager extends Fragment {
 
         imageView = rootView.findViewById(R.id.image_pager);
 
+        Glide.clear(imageView);
+        Glide.with(this)
+                .load(imageUrl)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .dontAnimate()
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model,
+                                                   Target<GlideDrawable> target,
+                                                   boolean isFromMemoryCache, boolean isFirstResource) {
+                        Bitmap bitmap = ((GlideBitmapDrawable) resource.getCurrent()).getBitmap();
+                        Palette palette = Palette.generate(bitmap);
+                        int defaultColor = 0xFF333333;
+                        int color = palette.getDarkMutedColor(defaultColor);
+                        imageView.setBackgroundColor(color);
+                        return false;
+                    }
+                })
+                .into(imageView);
+
+
         return rootView;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-//        Glide.clear(imageView);
-//        Glide.with(this)
-//                .load(imageUrl)
-//                .diskCacheStrategy(DiskCacheStrategy.ALL)
-//                .dontAnimate()
-//                .listener(new RequestListener<String, GlideDrawable>() {
-//                    @Override
-//                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
-//                        return false;
-//                    }
-//
-//                    @Override
-//                    public boolean onResourceReady(GlideDrawable resource, String model,
-//                                                   Target<GlideDrawable> target,
-//                                                   boolean isFromMemoryCache, boolean isFirstResource) {
-////                        Bitmap bitmap = ((GlideBitmapDrawable) resource.getCurrent()).getBitmap();
-////                        Palette palette = Palette.generate(bitmap);
-////                        int defaultColor = 0xFF333333;
-////                        int color = palette.getDarkMutedColor(defaultColor);
-////                        viewHolder.itemView.setBackgroundColor(color);
-//                        return false;
-//                    }
-//                })
-//                .into(imageView);
-
     }
 
     public int getPageNumber() {
